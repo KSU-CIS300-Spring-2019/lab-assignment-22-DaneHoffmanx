065f5f349bdb66fcb5530cc4f5484a2e1a09d932
danehoff <danehoff423@gmail.com>
2019-03-25 14:11:59 -0500

    Lab 22

---
+++ Ksu.Cis300.TrieLibrary/TrieWithNoChildren.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ksu.Cis300.TrieLibrary
+{
+    public class TrieWithNoChildren : ITrie
+    {
+        /// <summary>
+        /// Indicates whether the trie rooted at this node contains the empty string.
+        /// </summary>
+        private bool _hasEmptyString = false;
+
+        /// <summary>
+        /// Adds the given string to the trie rooted at this node.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public ITrie Add(string s)
+        {
+            if (s == "")
+            {
+                _hasEmptyString = true;
+                return this;
+            }
+            else
+            {
+                return new TrieWithOneChild(s, _hasEmptyString);
+            }
+        }
+
+        /// <summary>
+        /// Gets whether the trie rooted at this node contains the given string.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public bool Contains(string s)
+        {
+            if (s == "")
+            {
+                return _hasEmptyString;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Ksu.Cis300.TrieLibrary/TrieWithOneChild.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ksu.Cis300.TrieLibrary
+{
+    public class TrieWithOneChild : ITrie
+    {
+        /// <summary>
+        /// Indicates whether the trie rooted at this node contains the empty string.
+        /// </summary>
+        private bool _hasEmptyString;
+
+        /// <summary>
+        /// This node's only child.
+        /// </summary>
+        private ITrie _child;
+
+        /// <summary>
+        /// The label of the child.
+        /// </summary>
+        private char _label;
+
+        /// <summary>
+        /// Constructs a trie containing s and possibly the empty string.
+        /// If s is empty or contains any character other than a lower-case English letter,
+        /// throws an ArgumentException.
+        /// </summary>
+        /// <param name="s">The string to be included in the trie.</param>
+        /// <param name="hasEmptyString">Indicates whether the trie should contain the empty string.</param>
+        public TrieWithOneChild(string s, bool hasEmptyString)
+        {
+            if (s == "" || s[0] < 'a' || s[0] > 'z')
+            {
+                throw new ArgumentException();
+            }
+            _hasEmptyString = hasEmptyString;
+            _label = s[0];
+            _child = new TrieWithNoChildren().Add(s.Substring(1));
+        }
+
+        /// <summary>
+        /// Adds the given string to the trie rooted at this node.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public ITrie Add(string s)
+        {
+            if (s == "")
+            {
+                _hasEmptyString = true;
+                return this;
+            }
+            else if (s[0] == _label)
+            {
+                _child = _child.Add(s.Substring(1));
+                return this;
+            }
+            else
+            {
+                return new TrieWithManyChildren(s, _hasEmptyString, _label, _child);
+            }
+        }
+
+        /// <summary>
+        /// Gets whether the trie rooted at this node contains the given string.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public bool Contains(string s)
+        {
+            if (s == "")
+            {
+                return _hasEmptyString;
+            }
+            else if (s[0] == _label)
+            {
+                return _child.Contains(s.Substring(1));
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
